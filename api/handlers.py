1ef92615e70481d392e27b0c58d8f5d2b3f83e2f
GennadyBr <gennady.budazhapov@gmail.com>
2023-12-30 12:51:28 +0300

    session db

--- api/handlers.py
+++ api/handlers.py
@@ -1,6 +1,5 @@
-from fastapi import APIRouter
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from db.dals import UserDAL
 from db.session import async_session, get_db
@@ -9,18 +8,24 @@ from api.models import ShowUser, UserCreate
 user_router = APIRouter()
 
 
-async def _create_new_user(body: UserCreate) -> ShowUser:
+async def _create_new_user(body: UserCreate, db: AsyncSession) -> ShowUser:
     """Create a new user, flush into DB and return it as tuned ShowUser"""
-    async with async_session() as session:
+    async with db as session:
         async with session.begin():
             user_dal = UserDAL(session)
             user = await user_dal.create_user(
-                name=body.name, surname=body.surname, email=body.email,
+                name=body.name,
+                surname=body.surname,
+                email=body.email,
             )
-            return ShowUser(name=user.name, surname=user.surname, email=user.email)
+            return ShowUser(
+                name=user.name,
+                surname=user.surname,
+                email=user.email,
+            )
 
 
 @user_router.post("/", response_model=ShowUser)
-async def create_user(body: UserCreate, db: AsyncSession = Depends) -> ShowUser:
+async def create_user(body: UserCreate, db: AsyncSession = Depends(get_db)) -> ShowUser:
     """Router for creating a new user, flush into DB and return it as tuned ShowUser"""
-    return await _create_new_user(body)
+    return await _create_new_user(body, db)
--- db/session.py
+++ db/session.py
@@ -8,7 +8,7 @@ engine = create_async_engine(settings.REAL_DATABASE_URL, future=True, echo=True)
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 async def get_db() -> Generator:
-    """create a database async session via yield"""
+    """Dependency for getting database async session via yield"""
     try:
         session: AsyncSession = async_session()
         yield session
