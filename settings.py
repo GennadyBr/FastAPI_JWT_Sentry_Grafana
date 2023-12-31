ac1a5f96a05c384ba05af9183d44b52d280af5f8
GennadyBr <gennady.budazhapov@gmail.com>
2023-12-31 14:29:33 +0300

    initial tests

--- settings.py
+++ settings.py
@@ -7,3 +7,10 @@ REAL_DATABASE_URL = env_path(
     "REAL_DATABASE_URL",
     default="postgresql+asyncpg://postgres:postgres@0.0.0.0:5432/postgres"
 )
+
+TEST_DATABASE_URL = env_path(
+    "TEST_DATABASE_URL",
+    default="postgresql+asyncpg://postgres_test:postgres_test@0.0.0.0:5433/postgres_test"
+)
+
+TABLES_4_CLEANING = ["users"]
